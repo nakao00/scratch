4e7473d794481a488a9d8081d504ed86a0fdd4f7
nakao00 <agnakao@gmail.com>
2015-11-28 20:28:27 +0900

    ok

--- Remote001.js
+++ Remote001.js
@@ -1,10 +1,41 @@
-var vbHide = 0             //
-var vbNormalFocus = 1      //
-var vbMinimizedFocus = 2   //
-var vbMaximizedFocus = 3   //
-var vbNormalNoFocus = 4    //
-var vbMinimizedNoFocus = 6 //
+/* Extension demonstrating a blocking reporter block */
+/* Sayamindu Dasgupta <sayamindu@media.mit.edu>, May 2014 */
 
-var objWShell = new ActiveXObject("WScript.Shell")
 
-objWShell.Run("%windir%\\system32\\calc.exe", vbNormalFocus, false)
+new (function() {
+    var ext = this;
+
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.get_temp = function(location, callback) {
+        // Make an AJAX call to the Open Weather Maps API
+		var vbHide = 0             //
+		var vbNormalFocus = 1      //
+		var vbMinimizedFocus = 2   //
+		var vbMaximizedFocus = 3   //
+		var vbNormalNoFocus = 4    //
+		var vbMinimizedNoFocus = 6 //
+
+		var objWShell = new ActiveXObject("WScript.Shell")
+
+		objWShell.Run("%windir%\\system32\\calc.exe", vbNormalFocus, false)
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Weather extension', descriptor, ext);
+})();
+
